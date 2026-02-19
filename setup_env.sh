618e2281f794428c0de5e01b7b899327d6dcad87
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2026-02-19 15:46:55 +0200

    downgrade ray

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,7 @@ cd verl
 pip install -e .
 export CUDA_HOME="/opt/modules/nvidia-cuda-12.8.0"
 USE_MEGATRON=0 USE_SGLANG=0 bash scripts/install_vllm_sglang_mcore.sh 
-pip install numpy==2.2.0 transformers==4.57.6
+pip install numpy==2.2.0 transformers==4.57.6 ray==2.45.0
 cd ../ivon
 pip install -e .
 cd ../
