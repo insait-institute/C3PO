9da58a3ce16aa72ae6827ea4ebd9eb82a3a184e8
Vatsal Venkatkrishna <57312637+vatsal-kr@users.noreply.github.com>
2026-04-10 18:43:23 +0300

    up

--- setup_env.sh
+++ setup_env.sh
@@ -6,6 +6,7 @@ export CUDA_HOME="/opt/modules/nvidia-cuda-12.8.0"
 USE_MEGATRON=0 USE_SGLANG=0 bash scripts/install_vllm_sglang_mcore.sh 
 pip install numpy==2.2.0 transformers==4.57.6 vllm==0.11.0 torch==2.8.0
 pip install ray==2.45.0
+pip install math-verify
 cd ../ivon
 pip install -e .
 cd ../
